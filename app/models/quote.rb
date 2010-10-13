cecd4cdfa32a0845d23606728b84e28ead9d7698
Phillip Ridlen <phillip@ridlen.net>
2010-10-13 18:50:55 -0500

    update index view

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,10 @@
 class Quote < ActiveRecord::Base
   belongs_to :speaker
+  accepts_nested_attributes_for :speaker
   
   scope :normal, where(:offensive => false)
   scope :offensive, where(:offensive => true)
+  
+  validates_presence_of :body, :on => :create, :message => "can't be blank"
   
 end
