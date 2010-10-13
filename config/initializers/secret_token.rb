6118abbf25d84a7bfb8f7ffc5943f6c7bb0a3326
Phillip Ridlen <phillip@ridlen.net>
2010-10-13 00:54:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotastk::Application.config.secret_token = '085a00c47870d156425503315e52ec1e29b9ec0ef1db3122c8a5cc9014208becbb91fbbc37faf3611143bb1dcd0b8fe8eec3f3c709f0689cb605ff4fdbe2677e'
